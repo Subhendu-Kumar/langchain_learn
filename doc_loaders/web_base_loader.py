ae5e47d6686056518b5b9fa2eddd29c68ed13144
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2025-08-13 21:05:59 +0530

    loaders completed

---
+++ doc_loaders/web_base_loader.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import WebBaseLoader
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template="Answer the following question \n {question} from the following text - \n {text}",
+    input_variables=["question", "text"],
+)
+
+url = "https://www.flipkart.com/apple-macbook-air-m2-16-gb-256-gb-ssd-macos-sequoia-mc7x4hn-a/p/itmdc5308fa78421"
+loader = WebBaseLoader(url)
+
+docs = loader.load()
+
+
+chain = prompt | model | parser
+
+print(
+    chain.invoke(
+        {
+            "question": "What is the product that we are talking about?",
+            "text": docs[0].page_content,
+        }
+    )
+)
