fefa1cd7cc869736a398704db098bef90b2f04c1
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2025-08-13 20:16:12 +0530

    docs loaders

---
+++ doc_loaders/text_loader.py
@@ -0,0 +1,25 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+
+loader = TextLoader("cricket.txt", encoding="utf-8")
+doc = loader.load()
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template="Summarize the following poem in 50 words: \n\n {text}",
+    input_variables=["text"],
+)
+
+chain = prompt | model | parser
+
+result = chain.invoke({"text": doc[0].page_content})
+
+print(result)
